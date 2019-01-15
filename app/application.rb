7da42da2e962be79c36f004c8e44753499cd174c
Elise Phan <aliceconnic@gmail.com>
2019-01-15 05:15:03 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,6 +7,7 @@ class Application
     
     if req.path.match (/items/)
       item_name = req.path.split("/items/").last
+      item = @@items.find{|it| it.name == item_name}
       
 
       song_title = req.path.split("/songs/").last #turn /songs/Sorry into Sorry
