b74ba20856df4b5317908e9d50fa7869411fb3fd
Elise Phan <aliceconnic@gmail.com>
2019-01-15 05:10:58 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
     
-    if req.path =="/item/Item.name"
+    if req.path ="/item/"
       resp.write "Item.price"
     elsif
       
