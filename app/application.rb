841fdbc473fc3a1633344a98c385b527f8589b7b
Elise Phan <aliceconnic@gmail.com>
2019-01-15 05:13:51 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,8 +5,15 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
     
-    if req.path.match (/item/)
+    if req.path.match (/items/)
+      item_name = req.path.split
+      
 
+      song_title = req.path.split("/songs/").last #turn /songs/Sorry into Sorry
+      song = @@songs.find{|s| s.title == song_title}
+ 
+      resp.write song.artist
+      
     end
     
       resp.finish
