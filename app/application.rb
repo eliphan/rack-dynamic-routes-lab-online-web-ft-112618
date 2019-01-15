2fa7147c45f3a0d41ebbdd343e97b67e5fea333a
Elise Phan <aliceconnic@gmail.com>
2019-01-15 05:11:23 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,12 +6,7 @@ class Application
     req = Rack::Request.new(env)
     
     if req.path.match (/item/)
-      resp.write "Item.price"
-    elsif
-      
-    else
-      resp.write "Route not found"
-      resp.status = 404
+
     end
     
       resp.finish
